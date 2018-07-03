3b3eec848be0c017d830c462781174bc85bd2bf4
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-07-03 19:58:57 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+print("Hello!! First python program to implement a calculator")
+#adding two numbers
+print("Enter input data for addition")
+a=input()
+b=input()
+print'Sum is',a+b
+#subtraction of number
+print("Enter numbers to be subtracted")
+a=input()
+b=input()
+print"Difference is" ,a-b
+#Multiplication of numbers
+print("Enter numbers to be multiplied")
+a=input()
+b=input()
+print"Product is" ,a*b
+#Division of numbers
+print("Enter numbers to be divided")
+a=input()
+b=input()
+print"Remainder is" ,a%b
+print"Quotient" ,a/b
